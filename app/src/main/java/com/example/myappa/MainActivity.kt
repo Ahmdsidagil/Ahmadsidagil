3485f05f0cfd3e9821cb8d6388d32920341e515d
Ahmdsidagil <saidahmad1495@gmail.com>
2024-03-13 15:37:01 +0700

    Menganti Background

--- app/src/main/java/com/example/myappa/MainActivity.kt
+++ app/src/main/java/com/example/myappa/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
+    Surface(color = Color.Gray) {
         Text(
             text = "Hello $name! How are you?",
             fontSize = 30.sp,
